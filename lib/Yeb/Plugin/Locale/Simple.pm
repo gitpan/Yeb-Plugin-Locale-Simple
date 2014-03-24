bbd7efe3534c39ddb5f2e5aaf2e088f6bf1145bc
Torsten Raudssus <torsten@raudssus.de>
2014-03-24 23:35:07 +0000

    Import of GETTY/Yeb-Plugin-Locale-Simple-0.001 from CPAN.
    gitpan-cpan-distribution: Yeb-Plugin-Locale-Simple
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         GETTY/Yeb-Plugin-Locale-Simple-0.001.tar.gz
    gitpan-cpan-author:       GETTY
    gitpan-cpan-maturity:     released

---
+++ lib/Yeb/Plugin/Locale/Simple.pm
@@ -0,0 +1,44 @@
+package Yeb::Plugin::Locale::Simple;
+BEGIN {
+  $Yeb::Plugin::Locale::Simple::AUTHORITY = 'cpan:GETTY';
+}
+# ABSTRACT: Yeb Plugin for Locale::Simple connection
+$Yeb::Plugin::Locale::Simple::VERSION = '0.001';
+use Moo;
+use Locale::Simple;
+
+has app => ( is => 'ro', required => 1 );
+
+sub BUILD {
+  my ( $self ) = @_;
+  for my $func (@Locale::Simple::EXPORT) {
+    $self->app->register_function($func,sub { &{\&{$func}}(@_) });
+  };
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Yeb::Plugin::Locale::Simple - Yeb Plugin for Locale::Simple connection
+
+=head1 VERSION
+
+version 0.001
+
+=head1 AUTHOR
+
+Torsten Raudssus <torsten@raudss.us>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Torsten Raudssus.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
